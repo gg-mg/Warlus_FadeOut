08afa7136533b4ba27b646f4c2017dcc412d711a
gg-mg <ggichuki2017@gmail.com>
2019-12-03 14:16:10 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("h1").click(function() {
+      $("img").fadeOut();
+     
+    });
+
+  
+    $("p").click(function() {
+      $("img").fadeIn();
+     
+    });
+  });
